c21795a3661223bd66af4298c5c54a4b2237b8ab
birnybaum <149612550+birnybaum@users.noreply.github.com>
2023-11-15 10:12:25 -0500

    Delete lib/MultiPrior_WEB/setupWindows.bat

--- lib/MultiPrior_WEB/setupWindows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Command to create a Conda environment
-conda env create -f MultiPriors_env.yml --prefix .\MultiPriors_env
-
-:: Pause to see the output (optional, for debugging)
-pause
