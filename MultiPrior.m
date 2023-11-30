05ce3647013e2f110f3d669b7b28659401a128a8
birnybaum <149612550+birnybaum@users.noreply.github.com>
2023-11-30 11:42:50 -0500

    Update MultiPrior.m
    Checks if Segmentation Already Exists

--- MultiPrior.m
+++ MultiPrior.m
@@ -1,11 +1,17 @@
 function MultiPrior(subj)
 
-WARP_indiTPM(subj)
+[dirname,baseFilename] = fileparts(subj);
+if ~exist(fullfile(dirname, [baseFilename '_T1orT2_masks_MultiPrior_Segmentation.nii']), 'file')
 
-cd ..
-cd ..
-
-SEGMENT(subj) 
-
+    WARP_indiTPM(subj)
+    
+    cd ..
+    cd ..
+    
+    SEGMENT(subj) 
+else 
+    disp('MultiPrior Segmentation File: ')
+    disp([dirname, [baseFilename '_T1orT2_masks_MultiPrior_Segmentation.nii']])
+    disp('Already Exists. Skipping Segmentation...')
 end
-
+end
